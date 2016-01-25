fab8c68906a5a3edd0f7b32d65602ef69b3245aa
anonym0us3 <anonym0us3@users.noreply.github.com>
2016-01-24 23:54:32 -0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ActiveRecord::Base
 	has_secure_password
   has_many :posts
 
-   has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/default.jpeg"
+  has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/default.jpeg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
-  validates :first_name, :last_name, :current_city, :password, presence: true, length: { maximum: 50 }
+  validates :first_name, :last_name, :current_city, :password, 
+  	presence: true,
+  	length: { maximum: 50 }
 
 
 	def self.confirm(params)
